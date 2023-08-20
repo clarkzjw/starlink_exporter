7d30ca15433fe43b8bf16006743be1b5ee3e6782
clarkzjw <i@jinwei.me>
2023-08-20 01:57:35 -0400

    remove router metrics

--- grpc/get-protoset.sh
+++ grpc/get-protoset.sh
@@ -5,6 +5,8 @@ grpcurl -plaintext 192.168.100.1:9200 list
 
 # dish grpc
 grpcurl -plaintext -protoset-out protoset/dish.protoset 192.168.100.1:9200 describe SpaceX.API.Device.Device
+grpcurl -plaintext -d {\"get_status\":{}} 192.168.100.1:9200 SpaceX.API.Device.Device/Handle | grep softwareVersion | awk
+'{print $2}'
 
 # mesh
 #grpcurl -plaintext -protoset-out mesh.protoset 192.168.100.1:9200 describe SpaceX.API.Device.Mesh
