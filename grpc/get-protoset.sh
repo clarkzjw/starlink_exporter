fb6833866fd3ad8d3cf7bc9c8d401e1e14af0c43
clarkzjw <i@jinwei.me>
2023-08-20 02:59:36 -0400

    add docker stack

--- grpc/get-protoset.sh
+++ grpc/get-protoset.sh
@@ -2,14 +2,13 @@
 # requires https://github.com/fullstorydev/grpcurl
 
 grpcurl -plaintext 192.168.100.1:9200 list
+grpcurl -plaintext -d {\"get_status\":{}} 192.168.100.1:9200 SpaceX.API.Device.Device/Handle | grep softwareVersion | awk '{print $2}'
 
 # dish grpc
-grpcurl -plaintext -protoset-out protoset/dish.protoset 192.168.100.1:9200 describe SpaceX.API.Device.Device
-grpcurl -plaintext -d {\"get_status\":{}} 192.168.100.1:9200 SpaceX.API.Device.Device/Handle | grep softwareVersion | awk
-'{print $2}'
+grpcurl -plaintext -protoset-out dish.protoset 192.168.100.1:9200 describe SpaceX.API.Device.Device
 
 # mesh
-#grpcurl -plaintext -protoset-out mesh.protoset 192.168.100.1:9200 describe SpaceX.API.Device.Mesh
+grpcurl -plaintext -protoset-out mesh.protoset 192.168.100.1:9200 describe SpaceX.API.Device.Mesh
 
 # router
-#grpcurl -plaintext -protoset-out router.protoset 192.168.1.1:9000 describe SpaceX.API.Device.Device
+grpcurl -plaintext -protoset-out router.protoset 192.168.1.1:9000 describe SpaceX.API.Device.Device
